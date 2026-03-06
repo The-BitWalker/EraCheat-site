c8b96595d438ffbf52199b88908dbe055d64cb32
Student Niels Coert <niels@proton.me>
2026-03-06 18:51:12 +0100

    hopefully fixed turso..

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@libsql/client';
 
 export const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN!,
+  url: 'libsql://eracheat-the-bitwalker.aws-eu-west-1.turso.io',
+  authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NzI4MTc1NzUsImlkIjoiMDE5Y2M0MjctZjcwMS03NjgxLWI0ZDEtODJjNGY1MTExNWRjIiwicmlkIjoiZGUxYmIyOWQtYmI2OC00OThlLTg0NjItN2Q5YjI4ZGFjNWVmIn0.D-Q3svwJsCt3F9x6u8OTleioDTwcZNoVVut-D6TDwwGvRQ1_PqLSRbhF1wgrG8IVCqjII8tfoNASkP3gRSQaDA',
 });
